75e3fab84afbae72a6a6234e28addbee86a8f3c9
conorbell <78393011+dumpsterwater@users.noreply.github.com>
2023-10-30 08:07:45 -0400

    more button stuff

--- app.js
+++ app.js
@@ -1,2 +1,44 @@
 //play music 
-let playSound = () => new Audio("audio/elo.mp3").play();
+let playSound = () => new Audio('audio/elo.mp3').play();
+
+//create variable to store body in
+let body = document.querySelector('body');
+// let face = ''
+// face.height = '500px';
+// face.width = '500px';
+
+// let changeBackground = () => document.body.style.backgroundImage= "url('images/lilguy.jpg')";
+
+function changeBackground(){
+    const newBody = document.body.style.backgroundImage= "url('images/lilguy.jpg')";
+    const newFace = document.getElementById('me').src='images/oldman.jpg';
+    document.getElementById('me').style.maxHeight = '900px';
+    document.getElementById('me').style.maxWidth = '900px';
+    document.getElementById('me').style.marginBottom = '-350px';
+    // document.getElementById('me').style.marginBottom = ' /50px';
+
+
+    // return myMove();
+
+
+}
+
+//animate background function 
+// function myMove() {
+//     let id = null;
+//     const elem = body; 
+//     console.log(elem)
+//     let pos = 0;
+//     clearInterval(id);
+//     id = setInterval(frame, 5);
+//     function frame() {
+//       if (pos == 350) {
+//         clearInterval(id);
+//       } else {
+//         pos++;
+//         elem.style.top = pos + 'px';
+//         elem.style.left = pos + 'px';
+//       }
+//     }
+//     return frame;
+//   }
